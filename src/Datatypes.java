60060ae11bd5666dad4c4c3cc09e52fb771e9b35
yildirimog8 <100138219+yildirimog8@users.noreply.github.com>
2022-10-13 22:16:40 +0300

    java2022

---
+++ src/Datatypes.java
@@ -0,0 +1,9 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        double number = 12.4;
+        number = 12;
+        char kareter = 'a';
+        String name = "Oğuzhan";
+        System.out.println(name);
+    }
+}
